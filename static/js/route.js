087cf2f5d5e3d2d523e3cf38a857fce5296eb465
heeeete <mkoui@naver.com>
2024-02-13 20:33:02 +0900

    Fix : Login 페이지에서 레이아웃 뜨면 안 되는데 뜨는거 고침

--- static/js/route.js
+++ static/js/route.js
@@ -7,6 +7,41 @@ import Friends from './views/Friends.js';
 import Mypage from './views/Mypage.js';
 import Chat from './views/Chat.js';
 
+const routes = [
+  {
+    path: '/',
+    view: Login,
+  },
+  {
+    path: '/game',
+    view: Game,
+  },
+  {
+    path: '/friends',
+    view: Friends,
+  },
+  {
+    path: '/chat',
+    view: Chat,
+  },
+  {
+    path: '/mypage',
+    view: Mypage,
+  },
+  {
+    path: '/post',
+    view: Post,
+  },
+  {
+    path: '/post/:id',
+    view: PostView,
+  },
+  {
+    path: '/setting',
+    view: Setting,
+  },
+];
+
 const pathToRegex = path =>
   new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
 
@@ -30,40 +65,6 @@ const router = async () => {
   const mainTitle = document.querySelector('#main_title');
   navBar.style.display = location.pathname === '/' ? 'none' : 'block';
   mainTitle.style.display = location.pathname === '/' ? 'none' : 'block';
-  const routes = [
-    {
-      path: '/',
-      view: Login,
-    },
-    {
-      path: '/game',
-      view: Game,
-    },
-    {
-      path: '/friends',
-      view: Friends,
-    },
-    {
-      path: '/chat',
-      view: Chat,
-    },
-    {
-      path: '/mypage',
-      view: Mypage,
-    },
-    {
-      path: '/post',
-      view: Post,
-    },
-    {
-      path: '/post/:id',
-      view: PostView,
-    },
-    {
-      path: '/setting',
-      view: Setting,
-    },
-  ];
 
   const potentialMatches = routes.map(route => {
     return {
@@ -88,9 +89,14 @@ const router = async () => {
   const view = new match.route.view(getParams(match));
   if (location.pathname === '/')
     document.querySelector('#app').innerHTML = await view.getHtml();
-  else
+  else {
+    document.querySelector(
+      '#app',
+    ).innerHTML = `<div class="contentsContainer"></div>
+    <img id="headphoneImg" src="static/public/headphones.png" />`;
     document.querySelector('.contentsContainer').innerHTML =
       await view.getHtml();
+  }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -105,7 +111,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // 뒤로가기 앞으로가기 할때 해당 route 제대로 표시
 window.addEventListener('popstate', router);
-const navBar = document.querySelector('.nav');
-const mainTitle = document.querySelector('#main_title');
-navBar.style.display = location.pathname === '/' ? 'none' : 'block';
-mainTitle.style.display = location.pathname === '/' ? 'none' : 'block';
