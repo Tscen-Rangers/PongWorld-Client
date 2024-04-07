e1b6c1653bec880a6573421c2beac10d92d9fec6
heeeete <mkoui@naver.com>
2024-04-07 18:48:36 +0900

    Refactor : Rest API URL 변수로 변경

--- js/pages/myPage/Mypage.js
+++ js/pages/myPage/Mypage.js
@@ -353,16 +353,13 @@ ${
       );
       if (confirmed) {
         try {
-          const response = await fetch(
-            'http://localhost:8000/tcen-auth/delete/',
-            {
-              method: 'DELETE',
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': 'application/json',
-              },
+          const response = await fetch(`${API_UR}/tcen-auth/delete/`, {
+            method: 'DELETE',
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              'Content-Type': 'application/json',
             },
-          );
+          });
           if (response.ok) {
             alert('계정이 성공적으로 삭제되었습니다.');
             history.pushState(null, null, '/');
